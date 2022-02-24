99bea0efb3b98ba959c1aeecc6815a83cfb78b55
Jose Fletes <joseangelfletes@gmail.com>
2022-02-24 07:58:46 -0800

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById('dodger'); // select dodger element
+
+
+function moveDodgerLeft() { // function to move dodger left
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+
+  if(left > 0) { // make sure it doesnt go off grid
+    dodger.style.backgroundColor = "#f70000"
+    dodger.style.left = `${left - 10}px`;
+  }
+};
+
+function moveDodgerRight() { // function to move dodger right
+  const leftNumbers = dodger.style.left.replace('px', '');
+  const left = parseInt(leftNumbers, 10);
+  
+  if(left < 300) { // make sure it doesnt go off grid. Grid is 400px while dodger is 40px
+    dodger.style.backgroundColor = "#1cfc23"
+    dodger.style.left = `${left + 10}px`;
+  } 
+};
+
+document.addEventListener('keydown', (e) => { // event function checking which key was pressed and what to do with it
+  if(e.key === 'ArrowLeft') {
+    moveDodgerLeft();
+  } else if (e.key === 'ArrowRight') {
+    moveDodgerRight();
+  }
+});
